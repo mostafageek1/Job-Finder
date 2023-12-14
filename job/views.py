acb32baed473106c5c610b3f93a74f48e53459a7
Mostafa Ibnelkhattab <mostafageek1@gmail.com>
2023-12-14 18:05:00 +0100

    Pagination logic

--- job/views.py
+++ job/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import Job
+from django.core.paginator import Paginator
 
 
 def job_list(request):
     job_list = Job.objects.all()
-    context = {'jobs':job_list}
+    
+    paginator = Paginator(job_list, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+
+    context = {'jobs':page_obj}
     return render(request, 'job/job_list.html', context)
 
 def job_detail(request, id):
