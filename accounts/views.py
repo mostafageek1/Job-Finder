e18166a6bd47c0a50b88355021f8d73b168c9d06
Mostafa Ibnelkhattab <mostafageek1@gmail.com>
2023-12-16 22:27:37 +0100

    profile edit function

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect, render
-from .forms import SignupForm
+from django.urls import reverse
+from .forms import SignupForm, UserForm, ProfileForm
 from .models import Profile
 
 
@@ -23,4 +24,21 @@ def profile(request):
     return render(request, 'accounts/profile.html', {'profile':profile})
 
 def profile_edit(request):
-    return render(request, 'accounts/profile_edit.html')
+    profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        userform = UserForm(request.POST, instance=request.user)
+        profileform = ProfileForm(request.POST, request.FILES, instance=profile)
+
+        if userform.is_valid() and profileform.is_valid():
+            userform.save()
+            myprofile = profileform.save(commit=False)
+            myprofile.user = request.user
+            myprofile.save()
+            return redirect(reverse('accounts:profile'))
+        
+    else:
+        userform = UserForm(instance=request.user)
+        profileform = ProfileForm(instance=profile)
+
+    return render(request, 'accounts/profile_edit.html', {'userform':userform, 'profileform':profileform})
