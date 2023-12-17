2e58ea3963048319fdac29c43f3cc00cbed97ed6
Mostafa Ibnelkhattab <mostafageek1@gmail.com>
2023-12-17 20:13:14 +0100

    api adding

--- job/api.py
+++ job/api.py
@@ -5,8 +5,14 @@ from rest_framework.decorators import api_view
 
 
 @api_view(['GET'])
-def joblistapi(request):
+def job_list_api(request):
     all_jobs = Job.objects.all()
     data = JobSerializer(all_jobs, many=True).data
 
+    return Response({'data':data})
+
+@api_view(['GET'])
+def job_detail_api(request, id):
+    job_detail = Job.objects.get(id=id)
+    data = JobSerializer(job_detail).data
     return Response({'data':data})
--- job/urls.py
+++ job/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', views.job_list, name='job_list'),
     path('add/', views.add_job, name='add_job'),
     path('<str:slug>/', views.job_detail, name='job_detail'),
-    path('api/list', api.joblistapi, name='joblistapi'),
+    #api
+    path('api/jobs', api.job_list_api, name='job_list_api'),
+    path('api/jobs/<int:id>', api.job_detail_api, name='job_detail_api'),
 
 ]
