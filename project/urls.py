061c8226962a1895dbec5dee207691b6a6283f41
Mostafa Ibnelkhattab <mostafageek1@gmail.com>
2023-12-19 01:04:27 +0100

    adding home page

--- project/urls.py
+++ project/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('admin/', admin.site.urls),
+    path('', include('home.urls', namespace='home')),
     path('jobs/', include('job.urls', namespace='jobs')),
     path('contact/', include('contact.urls', namespace='contact')),
     path('api-auth/', include('rest_framework.urls')),
